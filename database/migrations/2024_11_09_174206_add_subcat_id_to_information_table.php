123be5578b96ad494b623ce033557a86f21ff6e5
agnaldohero99 <agnaldojunior@hero99.com.br>
2024-11-09 14:42:46 -0300

    adicionado migration

---
+++ database/migrations/2024_11_09_174206_add_subcat_id_to_information_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('information', function (Blueprint $table) {
+            $table->integer('subcat_id')->nullable();  // Adiciona o campo subcat_id
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('information', function (Blueprint $table) {
+            $table->dropColumn('subcat_id');  // Remove o campo subcat_id
+        });
+    }
+};
